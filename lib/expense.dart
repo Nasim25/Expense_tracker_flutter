b95b186d45e42622279a5b78536fb7e2e37f44d5
nasim114 <nasimuddin1140@gmail.com>
2023-09-21 00:00:31 +0600

    Create expense model

--- lib/expense.dart
+++ lib/expense.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
-class Expense extends StatefulWidget {
-  const Expense({super.key});
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
 
   @override
-  State<Expense> createState() => _ExpenseState();
+  State<Expenses> createState() => _ExpenseState();
 }
 
-class _ExpenseState extends State<Expense> {
+class _ExpenseState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Column(
         children: [
-          Text("nasim"),
-          Text("test"),
+          Text("The chart"),
+          Text("Expenses list.."),
         ],
       ),
     );
